2f437d13cd49bc9c11cd82735e33d0e297ef8b04
Haider Ali <73281382+nothaiderali@users.noreply.github.com>
2022-11-11 01:46:25 +0500

    update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!--Directory-Listing-Script-v1.2-->
+<!--Directory-Listing-Script-v1.3-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,6 +14,7 @@ audio, video, img{
   width: 19.2rem;
   margin: 1rem auto 1rem 0rem;
   border: 4px dotted #ccc;
+  display: block;
 }
 a, b{
   word-wrap: break-word;
@@ -25,6 +26,18 @@ a:link{
 </head>
 <body>
 <?php
+function ownurlencode($str){
+
+$search = array("+", "&", "#");
+$replace = array("%2B", "%26", "%23");
+
+return str_replace($search, $replace, $str);
+}
+
+function nbsp($str){
+return str_replace(" ", "&nbsp;", $str);
+}
+
 error_reporting(0);
 
 $maindir = ".";
@@ -46,17 +59,17 @@ $dir = $maindir.$_GET["path"];
 
 if ($opendirectory = opendir($dir)){
 
-echo "<b>Index&nbsp;of&nbsp;".substr($dir, $maindirl)."</b><br>
+echo "<b>".nbsp("Index of ".substr($dir, $maindirl))."</b><br>
 ";
 
 if ($dir !== $maindir){
 
-$pdir = substr($dir, 0,  strrpos($dir, '/'));
+$pdir = substr($dir, 0,  strrpos($dir, "/"));
 
 $pdir = substr($pdir, $maindirl);
 
-echo "<b>↩&nbsp;</b><a class='folders' href='?dark=".$dark."&media=".$media."&path=".$pdir."'>Parent Directory</a><br>
-";
+echo '<b>↩&nbsp;</b><a class="folders" href="?dark='.$dark.'&media='.$media.'&path='.ownurlencode($pdir).'">Parent Directory</a><br>
+';
 }
 
 while (($list = readdir($opendirectory)) !== false){
@@ -65,46 +78,51 @@ if ($list !== "." and $list !== ".."){
 
 if (is_dir($dir."/".$list)){
 
-array_push($folders,"<a class='folders' href='?dark=".$dark."&media=".$media."&path=".substr($dir, $maindirl)."/".$list."'>".$list."</a><br>
-");
+array_push($folders,'<a class="folders" href="?dark='.$dark.'&media='.$media.'&path='.ownurlencode(substr($dir, $maindirl).'/'.$list).'">'.nbsp($list).'</a><br>
+');
 
 }
 else{
 
+$link = ownurlencode($dir."/".$list);
+
 if ($media == "1")
 {
 
-$ext = strtolower(strrchr($list, '.'));
+$ext = strtolower(strrchr($list, "."));
 
-if ($ext == ".jpg" or $ext == ".jpeg" or $ext == ".png" or $ext == ".webp" or $ext == ".gif" or $ext == ".svg")
+
+if ($ext == '.jpg' or $ext == '.jpeg' or $ext == '.png' or $ext == '.webp' or $ext == '.gif' or $ext == '.svg')
 {
-$file = "
-<img src='".$dir."/".$list."'><br>
-";
+$file = '
+<img src="'.$link.'">
+';
 }
-else if ($ext == ".mp3" or $ext == ".m4a" or $ext == ".wav" or $ext == ".ogg" or $ext == ".aac")
+else if ($ext == '.mp3' or $ext == '.m4a' or $ext == '.wav' or $ext == '.ogg' or $ext == '.aac')
 {
-$file = "
+$file = '
 <audio controls>
-<source src='".$dir."/".$list."' type='audio/mp3'>
+<source src="'.$link.'">
 Your browser does not support the audio tag.
-</audio><br>
-";
+</audio>
+';
 }
-else if ($ext == ".mp4" or $ext == ".mkv" or $ext == ".mov" or $ext == ".webm")
+else if ($ext == '.mp4' or $ext == '.mkv' or $ext == '.mov' or $ext == '.webm')
 {
-$file = "
+$file = '
 <video controls>
-<source src='".$dir."/".$list."' type='video/mp4'>
+<source src="'.$link.'">
 Your browser does not support the video tag.
-</video><br>
-";
+</video>
+';
 }
 
 }
 
-array_push($files,"<a class='files' href='".$dir."/".$list."'>".$list."</a><br>
-".$file);
+array_push($files,'<a class="files" href="'.$link.'">'.nbsp($list).'</a><br>
+'.$file);
+
+unset($file);
 
 }
 }

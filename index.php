721946419bca119ffacb87c5f5279d2d436e83f3
Haider Ali <73281382+nothaiderali@users.noreply.github.com>
2022-11-11 01:50:09 +0500

    update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!--Directory-Listing-Script-v1.3-->
+<!--Directory-Listing-Script-v1.4-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,8 +13,11 @@ body{
 audio, video, img{
   width: 19.2rem;
   margin: 1rem auto 1rem 0rem;
-  border: 4px dotted #ccc;
+  border: 3px dotted #ccc;
   display: block;
+}
+p{
+  margin: 5px 0;
 }
 a, b{
   word-wrap: break-word;
@@ -26,7 +29,7 @@ a:link{
 </head>
 <body>
 <?php
-function ownurlencode($str){
+function ownurlencode($str) {
 
 $search = array("+", "&", "#");
 $replace = array("%2B", "%26", "%23");
@@ -34,8 +37,15 @@ $replace = array("%2B", "%26", "%23");
 return str_replace($search, $replace, $str);
 }
 
-function nbsp($str){
+function nbsp($str) {
 return str_replace(" ", "&nbsp;", $str);
+}
+
+function format_size($size) {
+$sizes = array(" Bytes", " KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB");
+if ($size == 0) { return('N/A'); } else {
+return (round($size/pow(1024, ($i = floor(log($size, 1024)))), 2) . $sizes[$i]);
+}
 }
 
 error_reporting(0);
@@ -53,102 +63,10 @@ $media = "0";
 if ($dark !== "0")
 $dark = "1";
 
-if ($_GET and array_key_exists("path", $_GET)){
-$dir = $maindir.$_GET["path"];
-}
-
-if ($opendirectory = opendir($dir)){
-
-echo "<b>".nbsp("Index of ".substr($dir, $maindirl))."</b><br>
-";
-
-if ($dir !== $maindir){
-
-$pdir = substr($dir, 0,  strrpos($dir, "/"));
-
-$pdir = substr($pdir, $maindirl);
-
-echo '<b>↩&nbsp;</b><a class="folders" href="?dark='.$dark.'&media='.$media.'&path='.ownurlencode($pdir).'">Parent Directory</a><br>
-';
-}
-
-while (($list = readdir($opendirectory)) !== false){
-
-if ($list !== "." and $list !== ".."){
-
-if (is_dir($dir."/".$list)){
-
-array_push($folders,'<a class="folders" href="?dark='.$dark.'&media='.$media.'&path='.ownurlencode(substr($dir, $maindirl).'/'.$list).'">'.nbsp($list).'</a><br>
-');
-
-}
-else{
-
-$link = ownurlencode($dir."/".$list);
-
-if ($media == "1")
-{
-
-$ext = strtolower(strrchr($list, "."));
-
-
-if ($ext == '.jpg' or $ext == '.jpeg' or $ext == '.png' or $ext == '.webp' or $ext == '.gif' or $ext == '.svg')
-{
-$file = '
-<img src="'.$link.'">
-';
-}
-else if ($ext == '.mp3' or $ext == '.m4a' or $ext == '.wav' or $ext == '.ogg' or $ext == '.aac')
-{
-$file = '
-<audio controls>
-<source src="'.$link.'">
-Your browser does not support the audio tag.
-</audio>
-';
-}
-else if ($ext == '.mp4' or $ext == '.mkv' or $ext == '.mov' or $ext == '.webm')
-{
-$file = '
-<video controls>
-<source src="'.$link.'">
-Your browser does not support the video tag.
-</video>
-';
-}
-
-}
-
-array_push($files,'<a class="files" href="'.$link.'">'.nbsp($list).'</a><br>
-'.$file);
-
-unset($file);
-
-}
-}
-}
-
-natcasesort($folders);
-foreach ($folders as $folder) {
-echo "<b>•&nbsp;</b>".$folder;
-}
-
-natcasesort($files);
-foreach ($files as $file) {
-echo "<b>•&nbsp;</b>".$file;
-}
-
-}
-else{
-echo "<b>Directory does not exist.<b>";
-}
-
 if ($dark == "0")
 echo "<style>
 body{
   background-color: white;
-}
-b{
   color: black;
 }
 a.folders:link {
@@ -171,9 +89,7 @@ else
 echo "<style>
 body{
   background-color: #1f1f1f;
-}
-b{
-  color: #f1f3f4;
+  color: white;
 }
 a.folders:link {
   color: #23bcf1;
@@ -189,6 +105,97 @@ a.files:visited {
 }
 </style>
 ";
+
+if ($_GET and array_key_exists("path", $_GET)){
+$dir = $maindir.$_GET["path"];
+}
+
+if ($opendirectory = opendir($dir)){
+
+if ($dir !== $maindir){
+
+echo "<b>".nbsp("Index of ".substr($dir, $maindirl))."</b>
+";
+
+$pdir = substr($dir, 0,  strrpos($dir, "/"));
+
+$pdir = substr($pdir, $maindirl);
+
+echo nbsp("<p>⤶ ").'<a class="folders" href="?dark='.$dark.'&media='.$media.'&path='.ownurlencode($pdir).'">Parent Directory</a></p>
+';
+}
+
+else {
+echo "<b>".nbsp("Index of /")."</b>
+";
+}
+
+
+while (($list = readdir($opendirectory)) !== false){
+
+if ($list !== "." and $list !== ".."){
+
+if (is_dir($dir."/".$list)){
+
+array_push($folders,'<a class="folders" href="?dark='.$dark.'&media='.$media.'&path='.ownurlencode(substr($dir, $maindirl).'/'.$list).'">'.nbsp($list).'</a>');
+
+}
+else{
+
+$link = ownurlencode($dir."/".$list);
+
+if ($media == "1")
+{
+
+$ext = strtolower(strrchr($list, "."));
+
+
+if ($ext == '.jpg' or $ext == '.jpeg' or $ext == '.png' or $ext == '.webp' or $ext == '.gif' or $ext == '.svg')
+{
+$file = '<img src="'.$link.'">';
+}
+else if ($ext == '.mp3' or $ext == '.m4a' or $ext == '.wav' or $ext == '.ogg' or $ext == '.aac')
+{
+$file = '<audio controls>
+<source src="'.$link.'">
+Your browser does not support the audio tag.
+</audio>';
+}
+else if ($ext == '.mp4' or $ext == '.mkv' or $ext == '.mov' or $ext == '.webm')
+{
+$file = '<video controls>
+<source src="'.$link.'">
+Your browser does not support the video tag.
+</video>';
+}
+
+}
+
+array_push($files,'<a class="files" href="'.$link.'">'.nbsp($list.'</a> ~ '.format_size(filesize($link))).$file);
+
+unset($file);
+
+}
+}
+}
+
+natcasesort($folders);
+foreach ($folders as $folder) {
+echo nbsp("<p>• ").$folder."</p>
+";
+}
+
+natcasesort($files);
+foreach ($files as $file) {
+echo nbsp("<p>• ").$file."</p>
+";
+}
+
+}
+else{
+echo "<b>Directory does not exist.<b>";
+}
+
 ?>
 <!--
 </body>
